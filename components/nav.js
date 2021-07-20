330718bfbee85261e5c73b8d1645c8a6967c4013
Brian Lemons <brian_lemons@hotmail.com>
2021-07-20 09:27:08 -0700

    adding front end changes and logout button

--- components/nav.js
+++ components/nav.js
@@ -4,14 +4,15 @@ export default function Nav() {
     return (
         <>
             <header className="flex items-center w-full h-24 p-6 border-t justify-left">
-                <h1 className="text-2xl font-semibold">Whiteboard Rubric</h1>
+                <h1 className="w-6/12 text-2xl font-semibold">Whiteboard Rubric</h1>
                 <nav className="flex justify-end w-full">
                     {/* <Link href="/">
                         <a class="mr-4">Admin View</a>
                     </Link> */}
-                    <Link href="/overview">
-                        <a>About us</a>
+                    <Link className="mr-2" href="/overview">
+                        <a className="ml-2 text-lg">About us</a>
                     </Link>
+                    <a className="ml-2 text-lg" href="/api/auth/logout">Logout</a>
                 </nav>
             </header>
         </>
--- pages/landing.js
+++ pages/landing.js
@@ -14,7 +14,7 @@ export default function Landing() {
 
     return (
         <>
-            <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-500">
+            <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-400">
                 <Header/>
                 <Nav/>
 
@@ -25,7 +25,7 @@ export default function Landing() {
                         Rubric
                     </h2>
                     </h1>
-                    <div className="flex px-6 py-10 mx-10 my-4 text-lg font-bold text-gray-400">
+                    <div className="flex px-6 py-10 mx-10 my-4 text-lg font-bold text-gray-300">
                         <button className="p-6 m-4 border-2">
                             <a href="/api/auth/login">
                                 Student Login
@@ -37,7 +37,7 @@ export default function Landing() {
                             </a>
                         </button>
                     </div>
-                    <DataContainer/>
+                    {/* <DataContainer/> */}
                 </main>
                     
                 <Footer/>
