e13446ce140f28c8f2434f61f5130b1df9384cd9
Brian Lemons <brian_lemons@hotmail.com>
2021-07-21 10:11:20 -0700

    adding ability to render json data onto page

--- pages/whiteboards.js
+++ pages/whiteboards.js
@@ -17,11 +17,49 @@ export default function Whitebaords() {
       const res = await axios.get('http://127.0.0.1:8000/api/v1/rubrics/')
       
       res.data.map(item => {
-          console.log('this is rubrics' + item.comments)
+        //   console.log('this is rubrics ' + item.comments)
+        //   console.dir(item)
+
+          const json_objects = {
+            proctor:  item.proctor,
+            time_start : item.time_start,
+            time_end : item.time_end,
+            challenge: item.challenge,
+            clarifying_question: item.clarifying_question,
+            inputs_outputs: item.inputs_outputs,
+            illustrate_problem: item.illustrate_problem,
+            optimal_structure: item.optimal_structure,
+            interpret_question_notes: item.interpret_question_notes,
+            working_algorithm: item.working_algorithm,
+            syntactically_correct: item.syntactically_correct,
+            idiomatically_correct: item.idiomatically_correct,
+            best_solution: item.best_solution,
+            solve_problem_notes: item.solve_problem_notes,
+            walkthrough_solution: item.walkthrough_solution,
+            big_o: item.big_o,
+            testing: item.testing,
+            analyze_solution_notes: item.analyze_solution_notes,
+            thought_process: item.thought_process,
+            terminology: item.terminology,
+            use_time: item.use_time,
+            overconfident: item.overconfident,
+            underconfident: item.underconfident,
+            whiteboard: item.whiteboard,
+            communicate_effectively_notes: item.communicate_effectively_notes,
+            total: item.total,
+            comments: item.comments,
+            created_at: item.created_at,
+            student: item.student
+
+          }
+          var text = document.getElementById('test')
+              text.append(
+                item.comments + ' ',
+                // item.comments[1]
+              )
         })
     }
 
-    // TODO for each key in the key value pair, generate a table row with the key and value as the cell 
 
 
     return (
@@ -47,7 +85,7 @@ export default function Whitebaords() {
                 </div>
 
             <>
-                <table class="table">
+                {/* <table class="table">
                     <tr>
                         <th>Name</th>
                     </tr>
@@ -55,7 +93,8 @@ export default function Whitebaords() {
                         
                     </tbody>
                     
-                </table>
+                </table> */}
+                <p id="test"></p>
             </>
 
 
