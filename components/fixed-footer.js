03fe87f478e4457af151c4a36ce13e72ddab18d8
Mason Aviles <mce.aviles@gmail.com>
2021-07-22 00:45:09 -0700

    add blue hover to footer

--- components/fixed-footer.js
+++ components/fixed-footer.js
@@ -2,7 +2,7 @@ export default function FixedFooter() {
     return (
         <>
             <footer className="fixed bottom-0 flex justify-end w-full h-10 pr-5 bg-gray-300 items-right">
-                <p className="flex items-center justify-center text-gray-500 hover:text-gray-900">Powered by Django RFW, Next.js, Tailwind.css, Elephant SQL</p>
+                <p className="flex items-center justify-center text-gray-500 hover:text-blue-600">Powered by Django RFW, Next.js, Tailwind.css, Elephant SQL</p>
             </footer>
         </>
     )
