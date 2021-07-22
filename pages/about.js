8ef42c4cb9ab4b737d84dc709e66a5a082156cbe
Brian Lemons <brian_lemons@hotmail.com>
2021-07-22 00:08:39 -0700

    adding pics and bios but for some reason they are in read only

--- pages/about.js
+++ pages/about.js
@@ -14,25 +14,26 @@ export default function About() {
 
             <div className="flex flex-col object-center max-w-xs m-auto">
               <div className="flex m-2 justify-items-end">
-                <img className="flex m-2 justify-items-end" src="https://via.placeholder.com/300x450"></img>
+                <img className="flex m-2 justify-items-end" src="/data/images/brian.jpg" width="300" height="450"></img>
               </div> 
               <div>
-                <p>Brian is a fullstack software developer specializing in Python and Javascrip. While serving in the United States Army; Brian gained a passion for building useful technologies that makes individuals lives easier. He wants to work for a company that values efficency and customer service, along with creating new ideas that can make people's lives easier.</p>
+                <p>Brian is a fullstack software developer specializing in Python and Javascript. While serving in the United States Army; Brian gained a passion for building useful technologies that makes individuals lives easier. He wants to work for a company that values efficency and customer service, along with creating new ideas that can make people's lives easier.</p>
               </div>
             </div>
 
             <div className="flex flex-col object-center max-w-xs m-auto">
               <div className="flex m-2 justify-items-end">
-                <img className="flex m-2 justify-items-end" src="https://via.placeholder.com/300x450"></img>
+                <img className="flex m-2 justify-items-end" src="data/images/mason.png" width="300" height="450"></img>
               </div> 
-              <p>Brian is a fullstack software developer specializing in Python and Javascrip. While serving in the United States Army; Brian gained a passion for building useful technologies that makes individuals lives easier. He wants to work for a company that values efficency and customer service, along with creating new ideas that can make people's lives easier.</p>
+              <p>Hey, I’m Mason Aviles. I’m a software developer and have been in the front end space for 6 years.  I believe in accessible and creative technology.  I’m really passionate about being in this space because I see myself as a crafter and this is new age crafting.  I love building, creating, and watching people interact with my work.
+              </p>
             </div>
 
             <div className="flex flex-col object-center max-w-xs m-auto">
               <div className="flex m-2 justify-items-end">
-                <img className="flex m-2 justify-items-end" src="https://via.placeholder.com/300x450"></img>
+                <img className="flex m-2 justify-items-end" src="data/images/tim.JPG" width="300" height="450"></img>
               </div> 
-              <p>Brian is a fullstack software developer specializing in Python and Javascrip. While serving in the United States Army; Brian gained a passion for building useful technologies that makes individuals lives easier. He wants to work for a company that values efficency and customer service, along with creating new ideas that can make people's lives easier.</p>
+              <p>TIM SORTEN YOUR DANG</p>
             </div>
 
             </main>
