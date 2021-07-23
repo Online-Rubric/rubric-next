7ffda32405ee53cad5de218e1a85c281b2cf9d4f
Mason Aviles <mce.aviles@gmail.com>
2021-07-22 23:18:25 -0700

    finalize loading screen

--- pages/loading.js
+++ pages/loading.js
@@ -1,15 +1,16 @@
-import Header from '../components/header'
-import Nav from '../components/nav'
-import FixedFooter from '../components/fixed-footer'
+import Header from '../components/header';
+import Nav from '../components/nav';
+import FixedFooter from '../components/fixed-footer';
+import loadingGif8 from '../data/images/loading-8.gif';
 
 export default function Loading() {
     return (
         <>
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-400">
+        <div className="flex flex-col items-center justify-start w-full min-h-screen bg-gray-400 ">
             <Header/>
             <Nav/>
-                <main className="container block w-full h-screen my-5 items-left">
-                    <h3 className="inline-block mr-3 text-xl">Loading...</h3>
+                <main className="container flex items-center justify-center w-full h-full my-5">
+                    <img src={loadingGif8.src} className="inline-block mt-20"/>
                 </main>
             <FixedFooter/>
         </div>
